974f715531662ef9e60b0de74f36c4f42065c0a8
marwantarek13579 <marwantarek13579@gmail.com>
2025-02-25 12:13:45 +0000

    create the project, add getX package, create splash view

---
+++ lib/features/splash/presentation/views/splashView.dart
@@ -0,0 +1,15 @@
+
+import 'package:bookly/features/splash/presentation/views/widgets/splashViewBody.dart';
+import 'package:flutter/material.dart';
+
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
---
+++ lib/features/splash/presentation/views/widgets/splashViewBody.dart
@@ -0,0 +1,25 @@
+
+import 'package:bookly/core/helpers/assets.dart';
+import 'package:flutter/material.dart';
+
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset(AssetsData.logo),
+          Text(
+            'Read free books',
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
