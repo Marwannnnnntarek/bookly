54d7b495d03dfe4f437e0d2a15111a21f7f7870b
Marwan Tarek <119248877+Marwannnnnntarek@users.noreply.github.com>
2025-08-27 14:48:10 +0300

    home remote data source.

---
+++ lib/features/home/data/data_sources/home_remote_data_source_imp.dart
@@ -0,0 +1,35 @@
+import 'package:bookly/core/services/api_services.dart';
+import 'package:bookly/features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:bookly/features/home/data/models/book_model/item.dart';
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
+
+class HomeRemoteDataSourceImp extends HomeRemoteDataSource {
+  final ApiServices apiServices;
+
+  HomeRemoteDataSourceImp(this.apiServices);
+  @override
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiServices.get(
+      endPoint: 'volumes?q=free&filter=free-ebooks&maxResults=20',
+    );
+    return getBooksList(data);
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() async {
+    var data = await apiServices.get(
+      endPoint:
+          'volumes?q=free&filter=free-ebooks&orderBy=newest&maxResults=20',
+    );
+    return getBooksList(data);
+  }
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+
+    for (var bookMap in data['items']) {
+      books.add(Item.fromJson(bookMap));
+    }
+    return books;
+  }
+}
