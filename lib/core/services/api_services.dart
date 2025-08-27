5af8ece9fe1e66b04f728c14d2cf1ead91766dac
Marwan Tarek <119248877+Marwannnnnntarek@users.noreply.github.com>
2025-08-27 14:28:52 +0300

    edit api services

--- lib/core/services/api_services.dart
+++ lib/core/services/api_services.dart
@@ -2,40 +2,13 @@ import 'package:dio/dio.dart';
 
 class ApiServices {
   final Dio dio;
-  static const String apiKey =
-      "AIzaSyBq7vzC8LoJ7lUt41vc0B44RvR_9BDtM6A"; // move to env/secure storage
+  final baseUrl = 'https://www.googleapis.com/books/v1/';
+  final apiKey = '&key=AIzaSyBq7vzC8LoJ7lUt41vc0B44RvR_9BDtM6A';
 
-  ApiServices({required this.dio}) {
-    dio.options = dio.options.copyWith(
-      baseUrl: 'https://www.googleapis.com/books/v1/',
-      connectTimeout: const Duration(seconds: 10),
-      receiveTimeout: const Duration(seconds: 20),
-    );
-  }
+  ApiServices({required this.dio});
 
-  Future<Map<String, dynamic>> get({
-    required String endPoint,
-    Map<String, dynamic>? queryParameters,
-    Options? options,
-  }) async {
-    try {
-      final response = await dio.get(
-        endPoint,
-        queryParameters: {
-          ...?queryParameters,
-          'key': apiKey, // always add API key
-        },
-        options: options,
-      );
-
-      final data = response.data;
-      if (data is Map<String, dynamic>) return data;
-
-      throw const FormatException('Response data is not a JSON object');
-    } on DioException catch (e) {
-      final status = e.response?.statusCode;
-      final message = e.message ?? 'Request failed';
-      throw Exception('GET $endPoint failed ($status): $message');
-    }
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    var response = await dio.get('$baseUrl$endPoint$apiKey');
+    return response.data;
   }
 }
